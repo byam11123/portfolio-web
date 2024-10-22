1700c1e319958d18faea356f8a653fbe1bcc84be
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-23 02:58:25 +0530

    Create projects._index.tsx

---
+++ app/routes/projects._index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BentoGrid from "~/components/BentoGrid";
+
+const Concerts: React.FC = () => {
+  return (
+    <div>
+      <BentoGrid />
+    </div>
+  );
+};
+
+export default Concerts;
