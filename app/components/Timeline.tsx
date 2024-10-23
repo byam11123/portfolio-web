0cfa9d05ae0b1e1e6fa26b47705ea78659e94997
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-23 20:32:10 +0530

    Update Timeline.tsx

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -30,7 +30,6 @@ const Timeline: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h2 className="text-3xl font-bold text-center mb-8">Work History</h2>
       <div className="relative border-l-4 border-purple-800">
         {experiences.map((experience, idx) => (
           <div className="mb-8 pl-8" key={idx}>
