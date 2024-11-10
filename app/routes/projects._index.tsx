9d1b68fc34138f80e7f0b36d0be95cfd213f8860
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-10 21:15:11 +0530

    Update projects._index.tsx

--- app/routes/projects._index.tsx
+++ app/routes/projects._index.tsx
@@ -1,12 +1,24 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import React from "react";
 import BentoGrid from "~/components/BentoGrid";
+import { getProjects, ProjectType } from "~/models/project.server";
 
-const Concerts: React.FC = () => {
+export const loader: LoaderFunction = async () => {
+  return json({
+    projects: await getProjects(),
+  });
+};
+
+const Projects: React.FC = () => {
+  const { projects } = useLoaderData<{ projects: ProjectType[] }>();
+
+  console.log(projects);
   return (
     <div>
-      <BentoGrid />
+      <BentoGrid projects={projects} />
     </div>
   );
 };
 
-export default Concerts;
+export default Projects;
