504703007300f98172abe5e157abeb9aaf899467
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-12 16:53:26 +0530

    Update projects.$id.tsx

--- app/routes/projects.$id.tsx
+++ app/routes/projects.$id.tsx
@@ -14,7 +14,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 const ProjectId: React.FC = () => {
   const { projectData } = useLoaderData<{ projectData: ProjectType }>();
-  console.log(projectData, "ProjectId");
+  // console.log(projectData, "ProjectId");
   return (
     <div>
       <ProjectDetails projectData={projectData} />
@@ -23,19 +23,3 @@ const ProjectId: React.FC = () => {
 };
 
 export default ProjectId;
-// const dummyProjectData = {
-//   title: "Awesome Portfolio Website",
-//   description:
-//     "A modern and responsive portfolio website to showcase my projects and skills. Built with React, Tailwind CSS, and GSAP for animations.",
-//   technologies: ["React", "Tailwind CSS", "GSAP", "TypeScript"],
-//   imageUrl:
-//     "https://images.unsplash.com/photo-1497294815431-9365093b7331?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   gallery: [
-//     "https://images.unsplash.com/photo-1497294815431-9365093b7331?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     "https://images.unsplash.com/photo-1497294815431-9365093b7331?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     "https://images.unsplash.com/photo-1497294815431-9365093b7331?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     "https://images.unsplash.com/photo-1497294815431-9365093b7331?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   ],
-//   liveUrl: "https://your-live-portfolio-url.com",
-//   githubUrl: "https://github.com/yourusername/portfolio-website",
-// };
