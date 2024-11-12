a3464ac4251128f34304796cf25bc93fbe25616e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-12 20:16:19 +0530

    Update BentoGrid.tsx

--- app/components/BentoGrid.tsx
+++ app/components/BentoGrid.tsx
@@ -1,18 +1,9 @@
 import { useGSAP } from "@gsap/react";
-import { Await, Link } from "@remix-run/react";
+import { Link } from "@remix-run/react";
 // eslint-disable-next-line import/no-named-as-default
 import gsap from "gsap";
 import { useRef } from "react";
 import { ProjectType } from "~/models/project.server";
-
-// interface GridItem {
-//   src: string;
-//   alt: string;
-//   title: string;
-//   description?: string;
-//   colSpan?: string;
-//   rowSpan?: string;
-// }
 
 export default function BentoGrid({ projects }: { projects: ProjectType[] }) {
   const gridRef = useRef<HTMLDivElement>(null);
@@ -40,36 +31,34 @@ export default function BentoGrid({ projects }: { projects: ProjectType[] }) {
           Bento Grid Layout with Images
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4" ref={gridRef}>
-          <Await resolve={projects}>
-            {projects?.map((item, index) => (
-              <Link
-                to={`${item.id}`}
-                key={index}
-                prefetch="intent"
-                className={`relative overflow-hidden rounded-2xl shadow-lg group cursor-pointer ${
-                  item?.colSpan ? item?.colSpan : ""
-                } ${item?.rowSpan ? item?.rowSpan : ""}`}
-              >
-                <img
-                  src={item?.image[0]}
-                  alt={item?.alt}
-                  className={`w-full ${
-                    item.colSpan ? "h-full" : "h-full"
-                  } object-cover`}
-                />
-                <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  <div className="absolute bottom-0 left-0 right-0 p-4">
-                    <h3 className="text-2xl font-bold text-white">
-                      {item?.title}
-                    </h3>
-                    {item?.description && (
-                      <p className="text-white">{item.description}</p>
-                    )}
-                  </div>
+          {projects?.map((item, index) => (
+            <Link
+              to={`${item.id}`}
+              key={index}
+              prefetch="intent"
+              className={`relative overflow-hidden rounded-2xl shadow-lg group cursor-pointer ${
+                item?.colSpan ? item?.colSpan : ""
+              } ${item?.rowSpan ? item?.rowSpan : ""}`}
+            >
+              <img
+                src={item?.mainImage}
+                alt={item?.alt}
+                className={`w-full ${
+                  item.colSpan ? "h-full" : "h-full"
+                } object-cover`}
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="absolute bottom-0 left-0 right-0 p-4">
+                  <h3 className="text-2xl font-bold text-white">
+                    {item?.title}
+                  </h3>
+                  {item?.description && (
+                    <p className="text-white">{item.description}</p>
+                  )}
                 </div>
-              </Link>
-            ))}
-          </Await>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
