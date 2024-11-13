cace8b51663dba69af2796bc52af5952ed3d04ae
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-13 21:54:27 +0530

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -13,6 +13,8 @@ import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 
 const ABORT_DELAY = 5_000;
+import dotenv from "dotenv";
+dotenv.config();
 
 export default function handleRequest(
   request: Request,
