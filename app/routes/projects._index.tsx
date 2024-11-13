f9b54df36ae958f17c6ffd4e2b90958eb7176443
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-13 21:54:20 +0530

    Update projects._index.tsx

--- app/routes/projects._index.tsx
+++ app/routes/projects._index.tsx
@@ -5,6 +5,7 @@ import gsap from "gsap";
 import { Await, defer, Link, useLoaderData } from "@remix-run/react";
 import { getProjects, ProjectType } from "~/models/project.server";
 import { useGSAP } from "@gsap/react";
+import Loader from "~/components/Loader/Loader";
 
 export const loader: LoaderFunction = () => {
   return defer({
@@ -13,16 +14,14 @@ export const loader: LoaderFunction = () => {
 };
 
 const Projects: React.FC = () => {
-  const { projects } = useLoaderData<{ projects: ProjectType[] }>();
+  const { projects } = useLoaderData<{ projects: Promise<ProjectType[]> }>();
   const gridRef = useRef<HTMLDivElement>(null);
-
-  // console.log(projects, "projects");
 
   useGSAP(() => {
     // Animate each grid item with a stagger effect when the component mounts
     gsap.fromTo(
       gridRef.current?.children,
-      { opacity: 0, y: 50 }, // Start state
+      { opacity: 0, y: 50 },
       {
         opacity: 1,
         y: 0,
@@ -36,7 +35,11 @@ const Projects: React.FC = () => {
   return (
     <div>
       <Suspense
-        fallback={<div className="text-white size-24 ">Loading...</div>}
+        fallback={
+          <div className="w-full h-screen flex items-center justify-center">
+            <Loader />
+          </div>
+        }
       >
         <div className="">
           <div className="container mx-auto px-4 py-8">
@@ -45,40 +48,45 @@ const Projects: React.FC = () => {
               className="grid grid-cols-1 md:grid-cols-4 gap-4"
               ref={gridRef}
             >
-              <Await resolve={projects} errorElement={<div>error</div>}>
-                {projects?.map((item, index) => (
-                  <Link
-                    to={`${item.id}`}
-                    key={index}
-                    prefetch="intent"
-                    className={`relative overflow-hidden rounded-2xl shadow-lg group cursor-pointer ${
-                      item?.colSpan ? item?.colSpan : ""
-                    } ${item?.rowSpan ? item?.rowSpan : ""}`}
-                  >
-                    <img
-                      src={item?.mainImage}
-                      alt={item?.alt}
-                      className={`w-full ${
-                        item.colSpan ? "h-full" : "h-full"
-                      } object-cover`}
-                    />
-                    <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                      <div className="absolute bottom-0 left-0 right-0 p-4">
-                        <h3 className="text-2xl font-bold text-white">
-                          {item?.title}
-                        </h3>
-                        {item?.description && (
-                          <p className="text-white">
-                            {item.description[0]?.content
-                              .split(" ")
-                              .slice(0, 6)
-                              .join(" ") + "...  "}
-                          </p>
-                        )}
-                      </div>
-                    </div>
-                  </Link>
-                ))}
+              <Await
+                resolve={projects as ProjectType[]}
+                errorElement={<p className="text-red-500">error</p>}
+              >
+                {(resolvedProjects: ProjectType[]) => (
+                  <>
+                    {resolvedProjects?.map((item, index) => (
+                      <Link
+                        to={`${item.id}`}
+                        key={index}
+                        prefetch="intent"
+                        className={`relative overflow-hidden rounded-2xl shadow-lg group cursor-pointer ${
+                          item?.colSpan || ""
+                        } ${item?.rowSpan || ""}`}
+                      >
+                        <img
+                          src={item?.mainImage}
+                          alt={item?.alt}
+                          className="w-full h-full object-cover"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                          <div className="absolute bottom-0 left-0 right-0 p-4">
+                            <h3 className="text-2xl font-bold text-white">
+                              {item?.title}
+                            </h3>
+                            {item?.description && (
+                              <p className="text-white">
+                                {item.description[0]?.content
+                                  .split(" ")
+                                  .slice(0, 6)
+                                  .join(" ") + "..."}
+                              </p>
+                            )}
+                          </div>
+                        </div>
+                      </Link>
+                    ))}
+                  </>
+                )}
               </Await>
             </div>
           </div>
