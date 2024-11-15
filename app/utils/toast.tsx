33e136936a23677bf043e37dea47d576e85e39f5
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-15 23:44:51 +0530

    Update toast.tsx

--- app/utils/toast.tsx
+++ app/utils/toast.tsx
@@ -1,20 +1,39 @@
 // utils/toast.tsx
 import toast from "react-hot-toast";
-import { IconCircleDashedCheck } from "@tabler/icons-react";
+import { IconCircleDashedCheck, IconCircleX } from "@tabler/icons-react";
 
 export function showSuccessToast(message: string) {
   toast.success(
     <div className="flex items-center space-x-2">
-      <IconCircleDashedCheck className="w-5 h-5 text-green-500 animate-bounce" />
       <span>{message}</span>
     </div>,
     {
       style: {
-        background: "#e6fffa",
-        border: "1px solid #81e6d9",
-        color: "#234e52",
+        background: "#3f3f46",
+        border: "1px solid #71717a",
+        color: "#ffffff",
       },
-      icon: null,
+      icon: (
+        <IconCircleDashedCheck className="w-10 h-10 text-green-500 animate-bounce" />
+      ),
+      duration: 5000,
+    }
+  );
+}
+
+export function showErrorToast(message: string) {
+  toast.error(
+    <div className="flex items-center space-x-2 ">
+      <span>{message}</span>
+    </div>,
+    {
+      style: {
+        background: "#3f3f46",
+        border: "1px solid #71717a",
+        color: "#ffffff",
+      },
+      icon: <IconCircleX className="w-5 h-5 text-red-500 animate-bounce" />,
+      duration: 5000,
     }
   );
 }
