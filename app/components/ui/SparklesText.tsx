dfbc0334e36ed32dd80c025b18371ea5aef22e55
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-17 00:04:42 +0530

    Update SparklesText.tsx

--- app/components/ui/SparklesText.tsx
+++ app/components/ui/SparklesText.tsx
@@ -117,7 +117,7 @@ export const SparklesText: React.FC<SparklesTextProps> = ({
         {sparkles.map((sparkle) => (
           <Sparkle key={sparkle.id} {...sparkle} />
         ))}
-        <strong>{text}</strong>
+        <strong className="text-black dark:text-white">{text}</strong>
       </span>
     </div>
   );
