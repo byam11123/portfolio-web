9117b8557a822214366cbd05fc16413e1d50759c
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-17 04:22:58 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -71,7 +71,7 @@ export default function App() {
       <Toaster position="top-center" reverseOrder={false} />
       <Outlet context={{ isIntroDone, setIsIntroDone }} />
       <Footer isIntroDone={isIntroDone} />
-      {!isMobile ? (
+      {isIntroDone && !isMobile ? (
         <FloatingDockDemo isIntroDone={isIntroDone} toggleTheme={toggleTheme} />
       ) : null}
     </Layout>
